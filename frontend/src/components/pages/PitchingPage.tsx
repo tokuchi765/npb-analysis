c3e04e5d71b53771c7eaeac915f50581ac74aa7f
tokuchi <hiroki1103.pon@gmail.com>
2021-02-07 21:21:18 +0900

    feat: チーム投手成績ページを追加

---
+++ frontend/src/components/pages/PitchingPage.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import GenericTemplate from '../templates/GenericTemplate';
+
+const PitchingPage: React.FC = () => {
+  return <GenericTemplate title="チーム投手成績ページ">チーム投手成績ページ</GenericTemplate>;
+};
+
+export default PitchingPage;

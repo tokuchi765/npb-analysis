e41473a4175e2ed48f66291425b9fefb2acfb04e
tokuchi <hiroki1103.pon@gmail.com>
2021-04-06 22:14:18 +0900

    refactor: 使用していないインポート要素を削除

--- frontend/src/components/common/TableComponent.tsx
+++ frontend/src/components/common/TableComponent.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { createStyles, lighten, makeStyles, Theme } from '@material-ui/core/styles';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';

d3f8f8c3cbfe56775dcd91c134c6f2ef96b32546
tokuchi <hiroki1103.pon@gmail.com>
2022-08-30 20:46:31 +0900

    feat: 使用していないメソッドを削除

--- team/team.go
+++ team/team.go
@@ -131,11 +131,3 @@ func (Interactor *TeamInteractor) InsertTeamBattings(csvPath string, league stri
 		}
 	}
 }
-
-func makeRange(min, max int) []int {
-	a := make([]int, max-min+1)
-	for i := range a {
-		a[i] = min + i
-	}
-	return a
-}

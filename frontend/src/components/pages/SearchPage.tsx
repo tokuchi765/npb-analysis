4f7ca52eea5ee894b36888d6bf3b286739cbfa10
tokuchi <hiroki1103.pon@gmail.com>
2023-01-22 22:32:49 +0900

    feat: 検索ページを追加

---
+++ frontend/src/components/pages/SearchPage.tsx
@@ -0,0 +1,8 @@
+import React, { useEffect, useState } from 'react';
+import GenericTemplate from '../templates/GenericTemplate';
+
+function SearchPage() {
+  return <GenericTemplate title="選手検索ページ">選手検索ページ</GenericTemplate>;
+}
+
+export default SearchPage;
